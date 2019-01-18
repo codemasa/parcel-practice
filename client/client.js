c2ee32c8feb5dd2ae701b52922010a98f7534395
Cody-Joe Abe <cody.masao@gmail.com>
2019-01-18 15:20:50 -0800

    added server side rendering. Added .babelrc, index.js, lib folder with ssr.mjs and client.js. Also added run scripts as well as some node modules

--- client/client.js
+++ client/client.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { HelmetProvider } from 'react-helmet-async';
+import { HelmetProvider } from 'react-helmet-async'
 import { BrowserRouter } from 'react-router-dom'
+import { rehydrateMarks } from 'react-imported-component';
+import importedComponents from './imported'; // eslint-disable-line
 
 const element = document.getElementById('app')
 const app = (
@@ -12,7 +14,16 @@ const app = (
     </BrowserRouter>
   </HelmetProvider>
 )
-ReactDOM.render(app, element)
+// In production, we want to hydrate instead of render
+// because of the server-rendering
+if (process.env.NODE_ENV === 'production') {
+  // rehydrate the bundle marks
+  rehydrateMarks().then(() => {
+    ReactDOM.hydrate(app, element);
+  });
+} else {
+  ReactDOM.render(app, element);
+}
 // Enable Hot Module Reloading
 if (module.hot) {
   module.hot.accept();
