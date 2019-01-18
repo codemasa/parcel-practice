415fb336a5d0bcf74b8daa6f003d5e36d9204168
Cody-Joe Abe <cody.masao@gmail.com>
2019-01-18 14:46:56 -0800

    added about, error, and loading pages with import-component support

--- client/App.jsx
+++ client/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import Helmet from 'react-helmet-async'
 import { GlobalStyles } from './styles/styles.js'
+import importComponent from 'react-imported-component';
+
+
+import Home from './pages/Home.jsx'
+import LoadingComponent from './pages/Loading'
+import ErrorComponent from './pages/Error'
+
+const About = importComponent(() => import("./pages/About"), {
+  LoadingComponent,
+  ErrorComponent
+});
+
 const App = () => (
   <React.Fragment>
     <GlobalStyles />
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/about" render={() => <About />} />
       <Redirect to="/" />
     </Switch>
   </React.Fragment>
